21414d886fbe4c681124856150f0d867d36b78ce
XxMattxX5 <116413060+XxMattxX5@users.noreply.github.com>
2025-05-09 18:35:59 -0400

    Finished Home Page

---
+++ frontend/src/app/components/global_components/HomeForumSection.tsx
@@ -0,0 +1,87 @@
+"use client";
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import styles from "@/app/styles/home.module.css";
+import Link from "next/link";
+import { useTheme } from "./ThemeProvider";
+
+import ExploreIcon from "@mui/icons-material/Explore";
+import CreateIcon from "@mui/icons-material/Create";
+import CommentIcon from "@mui/icons-material/Comment";
+
+const HomeForumSection = () => {
+  const { theme } = useTheme();
+  return (
+    <Grid className={styles.home_forum_container}>
+      <Typography component={"h2"}>Join The Game Hub Community</Typography>
+      <Typography component={"p"}>
+        Join the Game Hub community — a vibrant space where gamers and creators
+        come together to share their passion! Whether you're posting about your
+        favorite games, sharing general thoughts, or joining conversations by
+        commenting on others' posts, there's always something exciting
+        happening. Game Hub is the perfect place to connect, express yourself,
+        and be part of a growing community that lives and breathes games.
+      </Typography>
+      <Grid
+        className={styles.home_page_forum_section_content}
+        sx={{ color: theme === "dark" ? "white" : "var(--fontBlack)" }}
+      >
+        <Grid>
+          <Grid
+            className={styles.home_page_forum_box}
+            sx={{
+              backgroundColor: theme === "dark" ? "var(--lightGray)" : "white",
+            }}
+          >
+            <Grid>
+              <ExploreIcon />
+              <Typography component={"h3"}>Explore</Typography>
+            </Grid>
+            <Typography component={"p"}>
+              Explore a variety of posts and find something that sparks your
+              interest.
+            </Typography>
+            <Link href="/forum">Explore Posts</Link>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid
+            className={styles.home_page_forum_box}
+            sx={{
+              backgroundColor: theme === "dark" ? "var(--lightGray)" : "white",
+            }}
+          >
+            <Grid>
+              <CreateIcon />
+              <Typography component={"h3"}>Create a Post</Typography>
+            </Grid>
+            <Typography component={"p"}>
+              Connect with others who share your passion and chat about the
+              games you love.
+            </Typography>
+            <Link href="/forum/post/create-post">Create Post</Link>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid
+            className={styles.home_page_forum_box}
+            sx={{
+              backgroundColor: theme === "dark" ? "var(--lightGray)" : "white",
+            }}
+          >
+            <Grid>
+              <CommentIcon />
+              <Typography component={"h3"}>Comment</Typography>
+            </Grid>
+            <Typography component={"p"}>
+              Found a cool post? Join the conversation and share your thoughts.
+            </Typography>
+            <Link href="/forum">Start Chatting</Link>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default HomeForumSection;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import styles from "./styles/home.module.css";
-import { Grid } from "@mui/material";
+import { Grid, Typography, Link } from "@mui/material";
+import dynamic from "next/dynamic";
+import LoadingSpinner from "./components/global_components/LoadingSpinner";
+
+const HomeForumSection = dynamic(
+  () => import("@/app/components/global_components/HomeForumSection"),
+  {
+    loading: () => <LoadingSpinner spinnerSize={50} />,
+  }
+);
 
 export const metadata: Metadata = {
   title: "Game Hub",
@@ -11,7 +20,58 @@ const Home = () => {
   return (
     <Grid container>
       <Grid className={styles.home_landing_container} container>
-        <Grid component={"div"} className={styles.home_landing_box}></Grid>
+        <Grid component={"div"} className={styles.home_landing_box}>
+          <Grid className={styles.home_landing_box_content}>
+            <Grid>
+              <Typography component={"h1"}>
+                Your Ultimate
+                <br /> <span>Game</span> Database <br />
+                And Community <span>Hub</span>
+              </Typography>
+              <Typography component={"p"}>
+                Browse a massive library of games, find new games to play, and
+                jump into discussions with a passionate gaming community. Your
+                next favorite game—and the people who love it—are waiting.
+              </Typography>
+              <Link href="/games">Start Exploring Games</Link>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid className={styles.home_page_main_container}>
+        <Grid className={styles.home_page_game_section}>
+          <Grid className={styles.home_page_game_section_info}>
+            <Typography component={"h2"}>
+              Explore Our Vast Library Of Games
+            </Typography>
+            <Grid>
+              <img src="/images/games_image3.png" alt="Game Preview" />
+              <Typography component={"p"}>
+                Discover an ever-growing collection of games spanning every
+                genre and platform. Whether you're into action-packed
+                adventures, immersive RPGs, competitive multiplayer, or casual
+                puzzle games, our library has something for everyone. Dive in
+                and explore titles from indie gems to blockbuster hits—all in
+                one place. With new games added regularly, there's always
+                something fresh and exciting to play.
+              </Typography>
+            </Grid>
+            <Link href="/games">Start Searching Now</Link>
+          </Grid>
+          <Grid className={styles.home_page_game_section_image}>
+            <picture>
+              <source
+                media="(max-width: 1100px)"
+                srcSet="/images/games_image2.webp"
+              />
+
+              <img src="/images/games_image.webp" alt="Game Preview" />
+            </picture>
+          </Grid>
+        </Grid>
+        <Grid className={styles.home_page_forum_section}>
+          <HomeForumSection />
+        </Grid>
       </Grid>
     </Grid>
   );
