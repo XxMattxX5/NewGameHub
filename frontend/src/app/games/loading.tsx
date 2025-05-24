ab7a8c158a4c1df377bd8e9f880d65f00ed8dc94
XxMattxX5 <116413060+XxMattxX5@users.noreply.github.com>
2025-05-23 21:31:23 -0400

    Added loading skeleton for games page

--- frontend/src/app/games/loading.tsx
+++ frontend/src/app/games/loading.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Grid, CircularProgress } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Metadata } from "next";
+import styles from "@/app/styles/games.module.css";
 
 export const metadata: Metadata = {
   title: "Game Hub - Loading...",
@@ -8,8 +9,108 @@ export const metadata: Metadata = {
 
 export default function Loading() {
   return (
-    <Grid container id="loading_container">
-      <CircularProgress size={"100px"} />
+    <Grid container id={styles.games_loading_container}>
+      <Typography component={"p"} id={styles.games_loading_h1}></Typography>
+      <Grid id={styles.games_loading_content}>
+        <Grid id={styles.games_loading_top_rated}>
+          <Typography component={"p"}></Typography>
+          <Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+            <Grid></Grid>
+          </Grid>
+        </Grid>
+        <Grid id={styles.games_loading_games}>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid className={styles.games_loading_info}>
+              <Grid className={styles.games_loading_info_rating}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+
+              <Grid className={styles.games_loading_info_title}>
+                <Typography component={"p"}></Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
